1150b2f26c0312c83bbace285f4a34b47ab4a4d9
Rizve Ahmad <rizverizu1999@gmail.com>
2024-05-07 13:44:45 +0600

    Localisation
    Simple Multilingual Support and Localisation in ASP.NET Core Web Application with Razor Pages

---
+++ MultilingualApp/MultilingualApp/Pages/Lang.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MultiLingualApp.Pages
+{
+    public class LangModel : PageModel
+    {
+        public void OnGet()
+        {
+            string? culture = Request.Query["culture"];
+            Console.WriteLine("new selected language: " + culture);
+            if (culture != null)
+            {
+                Response.Cookies.Append(
+                    CookieRequestCultureProvider.DefaultCookieName,
+                    CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                    new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) }
+                );
+            }
+
+
+            string returnUrl = Request.Headers["Referer"].ToString() ?? "/";
+            Response.Redirect(returnUrl);
+
+        }
+    }
+}
